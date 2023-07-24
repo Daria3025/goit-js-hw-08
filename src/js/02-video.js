0b7167fa33a2723c68eca2b716fd15e4e8b5ccb6
Daria3025 <dtimofeeva191@icloud.com>
2023-07-24 18:55:51 +0300

    new

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -14,15 +14,15 @@ player
     .setCurrentTime(currentTime)
     .then(function (seconds) {
     // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
+    })
+    .catch(function (error) {
     switch (error.name) {
-      case 'RangeError':
+        case 'RangeError':
         // the time was less than 0 or greater than the video’s duration
         break;
 
-      default:
+        default:
         // some other error occurred
         break;
     }
-  });
+});
